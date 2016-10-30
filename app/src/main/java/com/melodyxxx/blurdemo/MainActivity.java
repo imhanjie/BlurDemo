831f2894bb689a43c8b6ef3d01035c2dd532dd5c
Melodyxxx <95hanjie@gmail.com>
2016-10-30 14:57:46 +0800

    commit

--- app/src/main/java/com/melodyxxx/blurdemo/MainActivity.java
+++ app/src/main/java/com/melodyxxx/blurdemo/MainActivity.java
@@ -1,7 +1,7 @@
 package com.melodyxxx.blurdemo;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -12,7 +12,7 @@ import android.widget.Toast;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
